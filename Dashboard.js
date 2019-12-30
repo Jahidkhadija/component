cd6c611a292c4556f2c8e856898630f23a4be10d
Jahidkhadija <56035956+Jahidkhadija@users.noreply.github.com>
2019-12-30 21:47:07 +0100

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Notifications from "./Notifications.js";
+import ProjectList from "./ProjectList.js";
+class Dashboard extends React.Component
+{
+	render()
+	{
+		return(
+		<div className="dashboard container">
+				<div className="row">
+				<div className="col s12 m6"><ProjectList/><div/>
+				<div className="col s12 m5 offset-m1"><div/>
+				<Notifications/>
+			</div>
+		</div>
+		);
+	}
+}
+export default Dashboard;
